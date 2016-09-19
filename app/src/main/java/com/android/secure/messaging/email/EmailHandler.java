ca0d6e047c55d8da309e2c0f15640a362f032be6
Chris S <cbs5347@psu.edu>
2016-09-18 21:10:54 -0400

    Created new unit tests

---
+++ app/src/main/java/com/android/secure/messaging/email/EmailHandler.java
@@ -0,0 +1,32 @@
+package com.android.secure.messaging.email;
+
+/**
+ * Created by christophershirley on 9/18/16.
+ */
+public class EmailHandler {
+
+    EmailHandler()
+    {
+
+    }
+
+    boolean send(Email email)
+    {
+        return false;
+    }
+
+    boolean saveUniqueEmail(String uniqueEmailAddress)
+    {
+        return false;
+    }
+
+    String requestUniqueEmailAddress()
+    {
+        return null;
+    }
+
+    String getUniqueEmail()
+    {
+        return null;
+    }
+}
