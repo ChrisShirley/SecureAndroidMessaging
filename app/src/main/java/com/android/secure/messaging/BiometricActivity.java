65b124e6b3c35c3402e4e4f7672488733882bd36
Chris Shirley <chris.shirley@softwaretheoretic.com>
2016-10-14 18:33:10 -0400

    Added functionality to check for fingerprint scanner and bypass if necessary.

--- app/src/main/java/com/android/secure/messaging/BiometricActivity.java
+++ app/src/main/java/com/android/secure/messaging/BiometricActivity.java
@@ -2,18 +2,22 @@ package com.android.secure.messaging;
 
 import android.Manifest;
 import android.app.Activity;
+import android.app.AlertDialog;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.hardware.fingerprint.FingerprintManager;
 import android.os.CancellationSignal;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import com.android.secure.messaging.Biometrics.BiometricCipher;
 import com.android.secure.messaging.Biometrics.BiometricKeyGenerator;
 import com.android.secure.messaging.Biometrics.FingerprintHandler;
-import com.android.secure.messaging.Preferences.Preferences;
 import com.android.secure.messaging.Preferences.PreferencesHandler;
 import com.android.secure.messaging.email.EmailCommService;
 import com.android.secure.messaging.email.EmailGenerator;
@@ -21,20 +25,21 @@ import com.android.secure.messaging.email.RandomStringGenerator;
 
 public class BiometricActivity extends AppCompatActivity {
 
-    //final private String emailPrefName = "SAMEmail";
     private FingerprintManager fingerprintManager;
     private FingerprintManager.CryptoObject cryptoObject;
     private CancellationSignal cancellationSignal;
-    private BiometricKeyGenerator biometricKeyGenerator = new BiometricKeyGenerator();
-    private BiometricCipher biometricCipher;
-    private FingerprintHandler fingerprintHandler;
-    private EmailGenerator emailGenerator = new EmailGenerator();
+    private static Context context;
+    BiometricKeyGenerator biometricKeyGenerator = new BiometricKeyGenerator();
+    BiometricCipher biometricCipher;
+    FingerprintHandler fingerprintHandler;
+    EmailGenerator emailGenerator = new EmailGenerator();
     private String domain = "@secureandroidmessaging.com";
-    private String generatedEmail;
-    private String generatedPassword;
+    String generatedEmail;
+    String generatdPassword;
+    private static boolean inOnCreate = false;
 
     RandomStringGenerator rsg = new RandomStringGenerator();
-    Preferences preferencesHandler = new PreferencesHandler();
+    PreferencesHandler preferencesHandler = new PreferencesHandler();
     EmailCommService ecs = new EmailCommService();
 
 
@@ -42,49 +47,124 @@ public class BiometricActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_biometric);
-
+        inOnCreate = true;
+        context = this;
         fingerprintManager = (FingerprintManager) getSystemService(FINGERPRINT_SERVICE);
+        fingerprintHandler = new FingerprintHandler(this);
+        if(!checkBiometricStatus())
+            return;
+
+        ecs.execute();
+
         biometricKeyGenerator.generateKey();
         biometricCipher = new BiometricCipher(biometricKeyGenerator.getKeyStore(), biometricKeyGenerator.getBiometricKey());
 
+
         generatedEmail = rsg.generateRandomEmail(12);
-        generatedPassword = rsg.generateRandomPassword(10);
+        generatdPassword = rsg.generateRandomPassword(10);
 
         try {
-            //Generators random email and password
-            emailGenerator.execute(generatedEmail, generatedPassword);
-            //Sets generated email and password in shared preferences
-            preferencesHandler.setPreference(getApplicationContext(), preferencesHandler.emailPrefName, generatedEmail + domain);
-            preferencesHandler.setPreference(getApplicationContext(), preferencesHandler.passwordPrefName, generatedPassword);
+            emailGenerator.execute(generatedEmail, generatdPassword);
+            //preferencesHandler.setEmailAddress(getApplicationContext(),generatedEmail + domain);
+            //System.out.println("This is what is in the email file: " + preferencesHandler.getEmailAddress(getApplicationContext()));
         } catch (Exception e) {
             e.printStackTrace();
         }
+        //System.out.println("Key: " + biometricKeyGenerator.getBiometricKey());
+        // System.out.println("KeyStore: " + biometricKeyGenerator.getKeyStore());
 
         if (biometricCipher.initCipher()) {
-
+             //   System.out.println("Got into the biometricCipher.initCipher() if loop");
+            //Toast.makeText(this, "In the loop", Toast.LENGTH_LONG).show();
             cryptoObject = new FingerprintManager.CryptoObject(biometricCipher.getCipher());
-            fingerprintHandler = new FingerprintHandler(this);
             fingerprintHandler.startAuthentication(fingerprintManager, cryptoObject);
             cancellationSignal = fingerprintHandler.getCancellationSignal();
             ContextCompat.checkSelfPermission(this, Manifest.permission.USE_FINGERPRINT);
             fingerprintManager.authenticate(cryptoObject, cancellationSignal, 0, fingerprintHandler, null);
         }
+
+
+
     }
 
-    public void startHome(Context context) {
-        System.out.println("The email preference is: " + preferencesHandler.getPreference(context, preferencesHandler.emailPrefName));
-        System.out.println("The email preference is: " + preferencesHandler.getPreference(context, preferencesHandler.passwordPrefName));
-
-        if (preferencesHandler.getPreference(getApplicationContext(), preferencesHandler.emailPrefName) == null) {
-            System.out.println("Email Pref name is null");
-            System.out.println(preferencesHandler.getPreference(getApplicationContext(), preferencesHandler.emailPrefName));
-        } else {
-            System.out.println("Email pref name is not null");
-            System.out.println(preferencesHandler.getPreference(getApplicationContext(), preferencesHandler.emailPrefName));
+    private boolean checkBiometricStatus()
+    {
+        ContextCompat.checkSelfPermission(this, Manifest.permission.USE_FINGERPRINT);
+        if(!fingerprintHandler.hasScanner(fingerprintManager)) {
+            createBiometricAlert();
+            return false;
         }
+        else if(!fingerprintManager.hasEnrolledFingerprints()) {
+            createNoFingerPrintAlert();
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        if(!inOnCreate)
+            checkBiometricStatus();
+        else
+            inOnCreate=false;
+        //Toast.makeText(this, "Resuming!", Toast.LENGTH_LONG).show();
+
+    }
+
+    public void createNoFingerPrintAlert() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("No Fingerprints Detected");
+
+        // Set an EditText view to get user input
+
+
+        builder.setMessage("You must have at least one fingerprint on file." +
+                "Please exit the application, add fingerprints, and try again.").setNegativeButton("Exit", dialogClickListener);
+
+        final AlertDialog biometricDialog = builder.create();
+        biometricDialog.show();
+
+
+    }
+
+    public void createBiometricAlert() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("No Fingerprint Scanner Detected");
+
+        // Set an EditText view to get user input
+
+
+        builder.setMessage("Not having a fingerprint scanner increases your security risk." +
+                "Would you like to continue?").setPositiveButton("Continue", dialogClickListener)
+                .setNegativeButton("Exit", dialogClickListener);
+
+        final AlertDialog biometricDialog = builder.create();
+        biometricDialog.show();
+
+
+    }
+
+        DialogInterface.OnClickListener dialogClickListener = new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                switch (which){
+                    case DialogInterface.BUTTON_POSITIVE:
+                        startHome();
+                        break;
+
+                    case DialogInterface.BUTTON_NEGATIVE:
+                        finish();
+                        System.exit(0);
+                        break;
+                }
+            }
+        };
+
+    public static void startHome() {
         Intent i = new Intent(context, Home.class);
         context.startActivity(i);
-        ((Activity) context).setContentView(R.layout.activity_home);
+
 
     }
 
