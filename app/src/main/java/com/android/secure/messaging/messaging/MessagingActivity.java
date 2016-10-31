fd5e83a39b24f8567360307ff94af525e9710684
Chris Shirley <chris.shirley@softwaretheoretic.com>
2016-10-31 19:17:53 -0400

    Updating MessagingActivity with stubs.

--- app/src/main/java/com/android/secure/messaging/messaging/MessagingActivity.java
+++ app/src/main/java/com/android/secure/messaging/messaging/MessagingActivity.java
@@ -1,15 +1,17 @@
 package com.android.secure.messaging.messaging;
 
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
+import android.view.WindowManager;
 
 import com.android.secure.messaging.R;
 import com.android.secure.messaging.keys.Decrypt;
 import com.android.secure.messaging.keys.Keys;
+
+import java.io.UnsupportedEncodingException;
+import java.util.ArrayList;
+import java.util.List;
 
 public class MessagingActivity extends AppCompatActivity {
 
@@ -20,11 +22,44 @@ public class MessagingActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_SECURE,
+                WindowManager.LayoutParams.FLAG_SECURE);
         decrypt = new Decrypt(keys.getPrivateKey());
         setContentView(R.layout.activity_messaging);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+
+        //getIntent().getStringExtra("contact"));
         setSupportActionBar(toolbar);
+        List<String> messages = new ArrayList<>();
+        //Get messages from server and decrypt
+        //messages = getMessages();
+
+        //Add messages to database
+
+        //displayMessages
+        messages.add("dummy message");
 
 
+    }
+
+    private List<String> getMessages()
+    {
+        List<String> encryptedMessages = new ArrayList<>();
+        //Get messages from the server
+
+        return decryptMessages(encryptedMessages);
+
+    }
+
+    private List<String> decryptMessages(List<String> encryptedMessages)
+    {
+        List<String> decryptedMessages = new ArrayList<>();
+        for (String message : encryptedMessages)
+            try {
+                decryptedMessages.add(new String(decrypt.decrypt(message.getBytes("ISO-8859-1"))));
+            } catch (UnsupportedEncodingException e) {
+                e.printStackTrace();
+            }
+        return decryptedMessages;
     }
 }
