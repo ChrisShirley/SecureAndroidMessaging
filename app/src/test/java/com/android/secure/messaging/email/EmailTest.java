0e92ee98174673c8ad4a75fa361dc3700ede1bcc
Chris S <cbs5347@psu.edu>
2016-11-27 04:13:03 -0500

    Update Email package unit tests. Added messaging activity unit test

--- app/src/test/java/com/android/secure/messaging/email/EmailTest.java
+++ app/src/test/java/com/android/secure/messaging/email/EmailTest.java
@@ -12,23 +12,38 @@ import org.junit.Test;
 public class EmailTest {
 
     Email email;
+    final String TEST_TO = "testTo";
+    final String TEST_FROM = "testFrom";
+    final String TEST_MESSAGE = "testMessage";
+    final String TEST_TIMESTAMP = "testTimestamp";
+
 
     @Before
     public void setUp() throws Exception {
 
-
+        email = new Email("testTo","testFrom","testMessage","testTimestamp");
 
     }
 
     @Test
     public void getMessage() throws Exception {
 
-        TestCase.assertTrue(email.getMessage()!=null);
+        TestCase.assertEquals(email.getMessage(),TEST_MESSAGE);
     }
 
+    @Test
+    public void getTo() throws Exception {
+        TestCase.assertEquals(email.getTo(),TEST_TO);
+    }
 
+    @Test
+    public void getFrom() throws Exception {
+        TestCase.assertEquals(email.getFrom(),TEST_FROM);
+    }
 
-
-
+    @Test
+    public void getTimestamp() throws Exception {
+        TestCase.assertEquals(email.getTimestamp(),TEST_TIMESTAMP);
+    }
 
 }
